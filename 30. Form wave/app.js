b44a905a7e69879c025996af889acba3f29fbb89
Aditya282007 <unstoppableadi28@gmail.com>
2024-09-11 22:45:20 +0530

    Form Wave Animation

---
+++ 30. Form wave/app.js
@@ -0,0 +1,7 @@
+const allLables = document.querySelectorAll('.form-control label');
+
+allLables.forEach(label => {
+    label.innerHTML = label.innerText.split('').map((letter, idx) => 
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+    .join('');
+})
